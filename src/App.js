d793d572149290103760274c1a7fa50e4bcb7b36
Josiah Maina <josiah5115@gmail.com>
2018-07-26 18:02:15 -0600

    starting todo app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+import { HashRouter, switch, Route } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard/Dashboard';
+import ToDo from './components/ToDo/ToDo';
+
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <Switch>
+          <Route exact path='/' component={ Dashboard } />
+          <Route path='/todo' component={ ToDo } />
+          <Dashboard />
+        </Switch>
+      </HashRouter>
+   
+
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      input: ''
+    };
+
+  }
+ 
+
+render() {
+  
+ }
+ 
+}
